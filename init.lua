dce4354982f0eded6e1f3781ced57220d121f85c
TEbinger <thomas.ebinger@gmx.de>
2017-11-26 21:59:05 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ minetest.register_on_joinplayer(function(player)
     if not laueft then
       laueft = true
       minetest.sound_play("christmassounds_mini-christmas-song", {
-        pos = {x = 0, y = 0, z = 0},
+        pos = {x = 166, y = 14, z = -122},
         max_hear_distance = 20,
         gain = 1,
         loop = true})
