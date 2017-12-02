7ddbebbf8c69c7f550e441b715553e93113ed691
TEbinger <thomas.ebinger@gmx.de>
2017-12-02 09:50:25 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 --Thanks to TalkLounge
-minetest.register_chatcommand("music", { privs = {privs = true}, 
-  func = function(name) minetest.sound_play("christmassounds_mini-christmas-song", {      pos = {x = 166, y = 14, z = -122},      max_hear_distance = 15,      gain = 0.6,      loop = true}) 
+minetest.register_chatcommand("music", { privs = {privs = true},
+  func = function(name) minetest.sound_play("christmassounds_mini-christmas-song", {      
+          pos = {x = 166, y = 14, z = -122},      
+          max_hear_distance = 15,      
+          gain = 0.6,      
+          loop = true}) 
   end}) 
 
 
