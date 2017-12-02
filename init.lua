2338ce795dbea85780ee6c6244f69ed963eeb915
TEbinger <thomas.ebinger@gmx.de>
2017-12-02 10:13:19 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,19 +6,19 @@ minetest.register_chatcommand("music", {
           pos = {x = 166, y = 14, z = -122},
           max_hear_distance = 15,
           gain = 0.6,
-          loop = true}) 
+          loop = true})
   end}) 
 
 --Kopiert vom Church Mod
 -- actually ring the bell
-church_bell.ring_church_bell_once = function()
+--church_bell.ring_church_bell_once = function()
 
-   for i,v in ipairs( church_bell_positions ) do
+   --for i,v in ipairs( church_bell_positions ) do
 -- print('Ringing bell at '..tostring( minetest.pos_to_string( v )));
-      minetest.sound_play( 'church_bell',
-        { pos = v, gain = 1.5, max_hear_distance = 300,});
-   end
-end
+     -- minetest.sound_play( 'church_bell',
+      --  { pos = v, gain = 1.5, max_hear_distance = 300,});
+  -- end
+--end
 
 
 
