a0b25b742f88fcd55a53c9ad024993cd2aa1b9f5
TEbinger <thomas.ebinger@gmx.de>
2017-12-02 09:45:03 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,13 +1,19 @@
 --Thanks to TalkLounge
-local laueft = false
+minetest.register_chatcommand("music", { privs = {privs = true}, 
+  func = function(name) minetest.sound_play("christmassounds_mini-christmas-song", {      pos = {x = 166, y = 14, z = -122},      max_hear_distance = 15,      gain = 0.6,      loop = true}) 
+  end}) 
+
+
+
+--local laueft = false
  
-minetest.register_on_joinplayer(function(player)
-    if not laueft then
-      laueft = true
-      minetest.sound_play("christmassounds_mini-christmas-song", {
-        pos = {x = 166, y = 14, z = -122},
-        max_hear_distance = 15,
-        gain = 0.6,
-        loop = true})
-    end
-end)
+--minetest.register_on_joinplayer(function(player)
+ --   if not laueft then
+  --    laueft = true
+   --   minetest.sound_play("christmassounds_mini-christmas-song", {
+    --    pos = {x = 166, y = 14, z = -122},
+     --   max_hear_distance = 15,
+      --  gain = 0.6,
+     --   loop = true})
+    --end
+--end)
